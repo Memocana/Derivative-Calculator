51d829b4275725b7f1a5dc2900829e7aab5ec05d
Memocana <Memo@Memows-MacBook-Air.local>
2014-12-15 20:11:28 +0200

    formatting modifications

--- src/derivative/calculator/DerivativeCalculator.java
+++ src/derivative/calculator/DerivativeCalculator.java
@@ -14,12 +14,28 @@ public class DerivativeCalculator {
         String str=console.nextLine();
         String spaceless = "";
         for (int i = 0; i < str.length(); i++) {
+            if(str.charAt(i) == 'x') {
+                if (i == 0 || !isNumeric(str.charAt(i-1)+"")){
+                    spaceless += "1";
+                }
+            }
             if(str.charAt(i) != ' ')
                 spaceless += str.charAt(i);
+            if(str.charAt(i) == 'x')
+                if (i == str.length() - 1 || str.charAt(i+1) != '^')
+                    spaceless += "^1";
         }
         System.out.println(derive(spaceless));
-        
-    }   
+    }  
+    public static boolean isNumeric(String str)  {  
+        try  {  
+            int d = Integer.parseInt(str);  
+        }  
+        catch(NumberFormatException nfe) {  
+            return false;  
+        }  
+        return true;  
+    }
     public static int findSign(String start, char end, String str) {
         for (int i = str.indexOf(start); i >= str.indexOf(end); i--) {
             if (str.charAt(i) == end)
@@ -29,21 +45,21 @@ public class DerivativeCalculator {
     }
     public static String derive(String str){
         System.out.println(str);
-        if (str.indexOf("(") == -1) { //anti parantez
-            if (str.indexOf("x") == -1)
-                return "0";
-            if (str.indexOf("+") != -1 )
-                if (str.indexOf("-") == -1 || str.indexOf("-") > str.indexOf("+"))
-                    return derive(str.substring(0,str.indexOf("+")))+" + " +derive(str.substring(str.indexOf("+")+1));
-            if (str.indexOf("-") != -1 )
-                    return derive(str.substring(0,str.indexOf("-")))+" - " +derive(str.substring(str.indexOf("-")+1));
-            if (str.indexOf("*") != -1) //product
-                return derive(str.substring(0,str.indexOf("*")))+"*"+str.substring(str.indexOf("*")+1) +" + " +str.substring(0,str.indexOf("*")) +"*"+derive(str.substring(str.indexOf("*")+1));
-            else if (str.indexOf("/") != -1) //quotient
-                return "("+derive(str.substring(0,str.indexOf("/")))+"*"+str.substring(str.indexOf("/")+1) +" - " +str.substring(0,str.indexOf("/")) +"*"+derive(str.substring(str.indexOf("/")+1))+") / (" +str.substring(str.indexOf("/")+1)+")^2" ;
-            else if (str.indexOf("^") != -1 && str.indexOf("^") - str.indexOf("x") == 1) //n X ^(n-1)
+        if (str.indexOf("x") == -1)
+            return "0";
+        if (str.indexOf("+") != -1 )
+            if (str.indexOf("-") == -1 || str.indexOf("-") > str.indexOf("+"))
+                return derive(str.substring(0,str.indexOf("+")))+" + " +derive(str.substring(str.indexOf("+")+1));
+        if (str.indexOf("-") != -1 )
+            return derive(str.substring(0,str.indexOf("-")))+" - " +derive(str.substring(str.indexOf("-")+1));
+        if (str.indexOf("*") != -1) //product
+            return derive(str.substring(0,str.indexOf("*")))+"*"+str.substring(str.indexOf("*")+1) +" + " +str.substring(0,str.indexOf("*")) +"*"+derive(str.substring(str.indexOf("*")+1));
+        else if (str.indexOf("/") != -1) //quotient
+            return "("+derive(str.substring(0,str.indexOf("/")))+"*"+str.substring(str.indexOf("/")+1) +" - " +str.substring(0,str.indexOf("/")) +"*"+derive(str.substring(str.indexOf("/")+1))+") / (" +str.substring(str.indexOf("/")+1)+")^2" ;
+        else if (str.indexOf("^") != -1 && str.indexOf("^") - str.indexOf("x") == 1) //n X ^(n-1)
                     return (Integer.parseInt(str.substring(0,str.indexOf("x")))*Integer.parseInt((str.substring(str.indexOf("^")+1))) + "x^(" + (Integer.parseInt(str.substring(str.indexOf("^")+1))-1))+")";
-        }
+        else if (str.indexOf("sin(") != -1)
+            return "cos(" ;
         return str;
     }
 }
